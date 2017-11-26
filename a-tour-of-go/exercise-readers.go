174a94607d7b0cda192226ce1a5a5b9c7b5a5175
mafuyuk <mafuyuk.m@gmail.com>
2017-11-26 15:13:15 +0900

    Added exercise readers

---
+++ a-tour-of-go/exercise-readers.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"golang.org/x/tour/reader"
+	"io"
+)
+
+type MyReader struct {
+	i int
+}
+
+func (mr MyReader) Read(b []byte) (int, error) {
+	if len(b) < mr.i {
+		return 0, io.EOF
+	}
+
+	for i := 0; i < len(b); i++ {
+		b[i] = 'A'
+	}
+
+	mr.i += len(b)
+	return len(b), nil
+}
