084913171b52bd71f7528258f40f32e12cf5e84d
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-06 12:28:28 +0900

    Added lesson2 znd 3

---
+++ learn-standard-package/bufio/reader/lesson2/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"strings"
+	"bufio"
+	"fmt"
+)
+
+func main() {
+	s1 := strings.NewReader(strings.Repeat("a", 16) + strings.Repeat("b", 16))
+	r := bufio.NewReaderSize(s1, 16)
+	b, _ := r.Peek(3)
+	fmt.Printf("%q\n", b)
+	r.Read(make([]byte, 16))
+	r.Read(make([]byte, 15))
+	fmt.Printf("%q\n", b)
+}
---
+++ learn-standard-package/bufio/reader/lesson3/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"strings"
+	"bufio"
+	"fmt"
+)
+
+func main() {
+	s1 := strings.NewReader("abcd")
+	r := bufio.NewReader(s1)
+	b := make([]byte, 3)
+	_, err := r.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%q\n", b)
+	s2 := strings.NewReader("efgh")
+	r.Reset(s2)
+	_, err = r.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%q\n", b)
+}
+
+
