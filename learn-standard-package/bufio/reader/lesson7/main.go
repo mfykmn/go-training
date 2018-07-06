6167e5dabb60c8189f0cdb66effeff73e08cacaf
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-06 15:06:52 +0900

    Added lesson

---
+++ learn-standard-package/bufio/reader/lesson7/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"strings"
+	"bufio"
+	"fmt"
+)
+
+type R struct{}
+
+func (r *R) Read(p []byte) (n int, err error) {
+	fmt.Println("Read")
+	copy(p, strings.Repeat("a", len(p)))
+	return len(p), nil
+}
+
+func main() {
+	r := new(R)
+	br := bufio.NewReaderSize(r, 16)
+	buf := make([]byte, 17)
+	n, err := br.Read(buf)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("read = %q, n = %d\n", buf[:n], n)
+	fmt.Printf("buffered = %d\n", br.Buffered())
+}
---
+++ learn-standard-package/bufio/reader/lesson8/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"strings"
+	"bufio"
+	"fmt"
+)
+
+func main() {
+	r := strings.NewReader("abcd")
+	br := bufio.NewReader(r)
+	byte, err := br.ReadByte()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%q\n", byte)
+	fmt.Printf("buffered = %d\n", br.Buffered())
+	err = br.UnreadByte()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("buffered = %d\n", br.Buffered())
+	byte, err = br.ReadByte()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%q\n", byte)
+	fmt.Printf("buffered = %d\n", br.Buffered())
+}
