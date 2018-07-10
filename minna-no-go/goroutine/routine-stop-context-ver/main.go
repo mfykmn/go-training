22bee2e84b80b4154f44770e6aacae781d82c341
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-10 18:23:36 +0900

    Updated

--- minna-no-go/goroutine/routine-stop-context-ver/main.go
+++ minna-no-go/goroutine/routine-stop-context-ver/main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"sync"
 	"fmt"
+	"context"
 )
 
-var wg sync.WaitGrou
+var wg sync.WaitGroup
 
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
 	queue := make(chan string)
 	for i := 0; i < 2; i++ {
 		wg.Add(1)
-		go fetchURL(queue)
+		go fetchURL(ctx, queue)
 	}
 
 
@@ -21,19 +23,19 @@ func main() {
 	queue <- "http://www.example.net/foo"
 	queue <- "http://www.example.net/bar"
 
-	close(queue)
+	cancel()
 	wg.Wait()
 }
 
-func fetchURL(queue chan string) {
+func fetchURL(ctx context.Context, queue chan string) {
 	for {
-		url, more := <-queue // closeされるとmoreがfalseになる
-		if more {
-			fmt.Println("fetching", url)
-		} else {
+		select {
+		case <-ctx.Done():
 			fmt.Println("worker exit")
 			wg.Done()
 			return
+		case url := <- queue:
+			fmt.Println("fetching", url)
 		}
 	}
 }
