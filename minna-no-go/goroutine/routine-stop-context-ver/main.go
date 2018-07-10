d3c115e6696b28a1aab2795afd687f46b2d9f72b
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-10 16:53:23 +0900

    Updated

---
+++ minna-no-go/goroutine/routine-stop-context-ver/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"sync"
+	"fmt"
+)
+
+var wg sync.WaitGrou
+
+
+func main() {
+	queue := make(chan string)
+	for i := 0; i < 2; i++ {
+		wg.Add(1)
+		go fetchURL(queue)
+	}
+
+
+	queue <- "http://www.example.com"
+	queue <- "http://www.example.net"
+	queue <- "http://www.example.net/foo"
+	queue <- "http://www.example.net/bar"
+
+	close(queue)
+	wg.Wait()
+}
+
+func fetchURL(queue chan string) {
+	for {
+		url, more := <-queue // closeされるとmoreがfalseになる
+		if more {
+			fmt.Println("fetching", url)
+		} else {
+			fmt.Println("worker exit")
+			wg.Done()
+			return
+		}
+	}
+}
