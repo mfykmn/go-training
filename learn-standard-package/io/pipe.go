77453bd99cbb0ed8c4b21f8ceba0d0f08efcca6d
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-10 16:31:15 +0900

    Updated

---
+++ learn-standard-package/io/pipe.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"io"
+	"fmt"
+	"bytes"
+)
+
+func main() {
+	r, w := io.Pipe()
+
+	go func() {
+		fmt.Fprint(w, "some text to be read\n")
+		w.Close()
+	}()
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r)
+	fmt.Print(buf.String())
+}
