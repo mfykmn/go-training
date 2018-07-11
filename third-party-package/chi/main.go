982f929b178a04abaa67afa13722b695b4146574
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-11 18:28:18 +0900

    Updated

--- third-party-package/chi/main.go
+++ third-party-package/chi/main.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 // curl http://localhost:8080/
 func main() {
 	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("root."))
 	})
