4d857bd2dbef3b86d4d6d0b7cde6ca35dec7807f
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-18 16:31:39 +0900

    Added os/signal

---
+++ standard-package/os/signal/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"time"
+	"fmt"
+	"os"
+	"syscall"
+	"os/signal"
+	"context"
+)
+
+// しぐなるのしゅるい
+
+// SIGHUP
+// SIGINT
+// SIGTERM
+// SIGQUIT
+// SIGILL
+// SGTRAP
+// SIGAMRT
+// SIGSTKFLT
+// SIGMET
+// SIGSYS
+
+func main() {
+	defer fmt.Println("done")
+
+	trapSignals := []os.Signal{
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+	}
+
+	// 受信するチャンネルの用意
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, trapSignals...)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go doMain(ctx)
+
+	sig := <-sigCh
+	fmt.Println("Got signal", sig)
+
+	cancel()
+
+	time.Sleep(3 * time.Second) // doMainのスレッドが終了したログが吐き出されるのを確認するために待つ
+}
+
+func doMain(ctx context.Context) {
+	defer fmt.Println("done infinite loop")
+	for {
+		select {
+		case <- ctx.Done():
+			fmt.Println("context done")
+			return
+		default:
+			time.Sleep(1 * time.Second)
+			fmt.Println("sleep")
+		}
+	}
+}
+
+
