aea12de36dd2fb967a6a98e92c5ee4ebebd1589c
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-18 15:22:18 +0900

    Added go-humanize

--- third-party-package/go-humanize/main.go
+++ third-party-package/go-humanize/main.go
@@ -15,4 +15,25 @@ func main() {
 		name,
 		humanize.Bytes(uint64(s.Size())),
 	)
+
+
+	// パースデモ
+	const maxBandWidth = "10MB"
+	conf := &Conf{}
+	conf.Parse(maxBandWidth)
+	fmt.Printf("Pased %s to %d", "10MB", conf.maxBandWidth)
+
+}
+
+type Conf struct {
+	maxBandWidth uint64
+}
+
+func (c *Conf)Parse(maxBandWidth string) {
+	if bw, err := humanize.ParseBytes(maxBandWidth); err != nil {
+		fmt.Println("Can not parse -max-bandwidth", err)
+		os.Exit(1)
+	} else {
+		c.maxBandWidth = bw
+	}
 }
