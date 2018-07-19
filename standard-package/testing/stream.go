a89979b0cfeb898e973bb66da3f737cded7db7ef
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-19 16:20:28 +0900

    Updated

--- standard-package/testing/stream.go
+++ standard-package/testing/stream.go
@@ -13,15 +13,31 @@ func multiWriter(in *os.File) {
 	gifDecodeTarget := new(bytes.Buffer)
 
 	w := io.MultiWriter(buf, gifDecodeTarget)
-	io.Copy(w, in)
+	_, err := io.Copy(w, in)
+	if err != nil {
+		panic(err)
+	}
 
-	image.Decode(buf)
-	gif.DecodeAll(gifDecodeTarget)
+	_, _, err = image.Decode(buf)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = gif.DecodeAll(gifDecodeTarget)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func seek(in *os.File) {
-	image.Decode(in)
+	_, _, err := image.Decode(in)
+	if err != nil {
+		panic(err)
+	}
 
 	in.Seek(0, 0) // インデックスを先頭に戻す
-	gif.DecodeAll(in)
+	_, err = gif.DecodeAll(in)
+	if err != nil {
+		panic(err)
+	}
 }
--- standard-package/testing/stream_test.go
+++ standard-package/testing/stream_test.go
@@ -3,18 +3,27 @@ package testing
 import (
 	"testing"
 	"os"
+	"log"
 )
 
-func streamBench(b *testing.B, file *os.File, f func(*os.File)) {
+func streamBench(b *testing.B, f func(*os.File)) {
 	b.ReportAllocs()
 	for i:=0; i< b.N; i++ {
+		file, err := os.Open("./sample.gif")
+		if err != nil {
+			log.Println("Failed file open")
+			return
+		}
+
 		f(file)
+		file.Close()
 	}
 }
 
+// $ go test -bench BenchmarkStream
 //                                   ループが実行された回数 １ループごとの所要時間 １ループごとのアロケーションされたバイト数 １ループごとのアロケーション回数
-// BenchmarkStream/MultiWriter-4             200000              5125 ns/op           38600 B/op         10 allocs/op
-// BenchmarkStream/Seek-4                        50          28012993 ns/op         6317885 B/op       3951 allocs/op
+// BenchmarkStream/MultiWriter-4                 20          54441881 ns/op        16391394 B/op       7903 allocs/op
+// BenchmarkStream/Seek-4                        20          55851878 ns/op        12626858 B/op       7890 allocs/op
 func BenchmarkStream(b *testing.B) {
 	benchCase := []struct {
 		name string
@@ -25,8 +34,6 @@ func BenchmarkStream(b *testing.B) {
 	}
 
 	for _, c := range benchCase {
-		file, _ := os.Open("./sample.gif")
-		b.Run(c.name, func(b *testing.B) { streamBench(b, file, c.f) })
-		file.Close()
+		b.Run(c.name, func(b *testing.B) { streamBench(b, c.f) })
 	}
 }
