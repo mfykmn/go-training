36b644df08991f692cf7f3b53b3a5b39d53179c5
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-19 14:54:42 +0900

    Added benchmark case

---
+++ standard-package/testing/cat_test.go
@@ -0,0 +1,38 @@
+package testing
+
+import "testing"
+
+func seed(n int) []string{
+	s := make([]string, 0, n)
+	for i := 0;i < n; i++ {
+		s = append(s, "a")
+	}
+	return s
+}
+
+func bench(b *testing.B, n int, f func(...string) string) {
+	b.ReportAllocs()
+	for i:=0; i< b.N; i++ {
+		f(seed(n)...)
+	}
+}
+
+func BenchmarkCat3(b *testing.B) { bench(b, 3, cat) }
+func BenchmarkBuf3(b *testing.B) { bench(b, 3, buf) }
+func BenchmarkCat100(b *testing.B) { bench(b, 100, cat) }
+func BenchmarkBuf100(b *testing.B) { bench(b, 100, buf) }
+func BenchmarkCat10000(b *testing.B) { bench(b, 10000, cat) }
+func BenchmarkBuf10000(b *testing.B) { bench(b, 10000, buf) }
+
+//$ go test -bench .
+//goos: darwin
+//goarch: amd64
+//pkg: github.com/mafuyuk/go-training/standard-package/testing
+
+//                ループが実行された回数 １ループごとの所要時間 １ループごとのアロケーションされたバイト数 １ループごとのアロケーション回数
+//BenchmarkCat3-4           10000000           127 ns/op              54 B/op                             3 allocs/op
+//BenchmarkBuf3-4           10000000           137 ns/op             163 B/op                             3 allocs/op
+//BenchmarkCat100-4           300000          4284 ns/op            7520 B/op                           100 allocs/op
+//BenchmarkBuf100-4          1000000          1198 ns/op            2160 B/op                             4 allocs/op
+//BenchmarkCat10000-4            200       6383734 ns/op        53327792 B/op                         10000 allocs/op
+//BenchmarkBuf10000-4          10000        102985 ns/op          211424 B/op                            11 allocs/op
