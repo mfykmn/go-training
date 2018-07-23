49c7970d1e88361dbe701f78391fdcf2545bd8eb
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-23 19:27:01 +0900

    Updated

---
+++ standard-package/encoding/csv/analytics.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"strings"
+	"encoding/csv"
+	"io"
+	"fmt"
+)
+
+var csvSource =
+`1111,"ooooo","aaaaaa"
+2222,"sssss","bbbbbb"
+3,"ddddddd","cccccc"
+`
+
+func main() {
+	reader := strings.NewReader(csvSource)
+	csvReader := csv.NewReader(reader)
+	for {
+		line, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(line)
+	}
+}
