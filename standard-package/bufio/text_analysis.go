1aa3d56ec48776ec756347c40b7006b29b53f81b
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-23 18:56:35 +0900

    Updated

---
+++ standard-package/bufio/text_analysis.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"strings"
+	"fmt"
+)
+
+var source = `１行目
+２行目
+３行目`
+
+func main () {
+	useBufioReader()
+	useBufioScanner()
+}
+
+func useBufioReader() {
+	reader := bufio.NewReader(strings.NewReader(source))
+	for {
+		line, err := reader.ReadString('\n')
+		fmt.Printf("%#v\n", line)
+		if err != nil {
+			break
+		}
+	}
+}
+
+// 分割文字が削除されることに注意
+func useBufioScanner() {
+	scanner := bufio.NewScanner(strings.NewReader(source))
+	for scanner.Scan() {
+		fmt.Printf("%#v\n", scanner.Text())
+	}
+}
