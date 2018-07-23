7daebe5e4c7e67a8dd28f04f63aae2098dabf203
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-23 16:03:38 +0900

    Updated

---
+++ standard-package/io/ImprementsReader/net_read.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net"
+	"net/http"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "ascii.jp:80")
+	if err != nil {
+		panic(err)
+	}
+	conn.Write([]byte("GET / HTTP/1.0\r\nHost: ascii.jp\r\n\r\n"))
+	res, err := http.ReadResponse(bufio.NewReader(conn), nil)
+	fmt.Println(res.Header)
+	defer res.Body.Close()
+	io.Copy(os.Stdout, res.Body)
+}
