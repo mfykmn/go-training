ee5457f58e0b5ba2d59f1b94de4bdc3c8f92c449
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-24 17:15:24 +0900

    Updated

---
+++ standard-package/path/filepath/httpServer.go
@@ -0,0 +1,39 @@
+package filepath
+
+import (
+	"os"
+	"io"
+	"net/http"
+	"path"
+	"path/filepath"
+)
+
+func main() {
+	http.HandleFunc("/", upload)
+
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		stream, header, err := r.FormFile("file")
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		p := filepath.Join("files", filepath.Base(header.Filename)) // filepathじゃないと\が送られても素通しでbad
+		println(p)
+		f, err := os.Create(p)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+
+		io.Copy(f, stream)
+		http.Redirect(w, r, path.Join("/file", p), 301)
+
+	} else {
+		http.Redirect(w, r, "/", 301)
+	}
+}
