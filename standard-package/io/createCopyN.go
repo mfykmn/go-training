1a2358fe202bc1a6c41c5cce8548d95228aacbdc
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-24 19:48:51 +0900

    Updated

--- standard-package/io/createCopyN.go
+++ standard-package/io/createCopyN.go
@@ -1,11 +1,33 @@
 package main
 
-import "io"
+import (
+	"io"
+	"strings"
+	"os"
+	"fmt"
+)
 
 // Goならわかるシステムプログラミングの回答 Q3.5
 func main() {
+	reader1 := strings.NewReader("mafuyuk")
+	reader2 := strings.NewReader("mafuyuk")
+
+	// もとからあるCopyN
+	written1, err := io.CopyN(os.Stdout, reader1, 3)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(written1)
+
+	// 自前実装のCopyN
+	written2, err := CopyN(os.Stdout, reader2, 3)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(written2)
 }
 
 func CopyN(dst io.Writer, src io.Reader, n int64) (written int64, err error) {
-	return 0, nil
+	reader := io.LimitedReader{src, n}
+	return io.Copy(dst, &reader)
 }
