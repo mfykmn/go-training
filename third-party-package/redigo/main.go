5220fbfc118d3aed00a3c766e89ed7475bc78c1d
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-24 13:03:43 +0900

    Updated

--- third-party-package/redigo/main.go
+++ third-party-package/redigo/main.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"flag"
 	"net/http"
 	"log"
 
 	"github.com/mafuyuk/go-training/third-party-package/redigo/redis"
 )
 
-type option struct {
-	host string
-	port string
-}
-
 func main() {
-	var opt = &option{}
-	flag.StringVar(&opt.host, "h", "", "Server host")
-	flag.StringVar(&opt.port, "p", "", "Server port")
-	flag.Parse()
-
 	client, err := redis.New("redis:6379")
 	if err != nil {
 		panic(err)
@@ -27,7 +16,7 @@ func main() {
 
 	http.HandleFunc("/",handler)
 	log.Println("Start server")
-	http.ListenAndServe(opt.host+opt.port, nil)
+	http.ListenAndServe(":8080", nil)
 }
 
 func handler(rw http.ResponseWriter, r *http.Request) {
