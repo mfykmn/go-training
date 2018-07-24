1ebc0b597d6c268ffeaafe36c29f5d7e1c4c4315
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-24 17:55:58 +0900

    Updated

--- standard-package/io/ImprementsReader/randRead.go
+++ standard-package/io/ImprementsReader/randRead.go
@@ -1,6 +1,23 @@
 package main
 
+import (
+	"os"
+	"crypto/rand"
+	"io"
+)
 
+// GoならわかるシステムプログラミングQ3.2の自分の回答
+// 実際の答えは randRead2.go のほう
 func main() {
+	file, err := os.Create("rand.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 
+	limitReader := io.LimitedReader{
+		R: rand.Reader,
+		N: 1024,
+	}
+	io.Copy(file, &limitReader)
 }
