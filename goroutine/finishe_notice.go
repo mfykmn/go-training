92afee7ef31734e8df3582b2fcfac2d2417b3925
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-25 12:58:18 +0900

    Updated

--- goroutine/finishe_notice.go
+++ goroutine/finishe_notice.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	fmt.Println("start sub()")
 	// 終了を受け取るためのチャネル
-	done := make(chan bool)
+	done := make(chan struct{})
 	go func() {
 		fmt.Println("sub() is finished")
 		// 終了の通知
-		done <- true
+		done <- struct{}{}
 	}()
 
 	//終了を待つ
