1f817f2272640e7e89a86006177e2991659f4ea4
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-07-27 17:46:15 +0900

    Updated

---
+++ standard-package/net/http_client_for_net.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net"
+	"net/http"
+	"bufio"
+	"net/http/httputil"
+	"fmt"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8888")
+	if err != nil {
+		panic(err)
+	}
+	request, err := http.NewRequest("GET", "http://localhost:8888", nil)
+	if err != nil {
+		panic(err)
+	}
+	request.Write(conn)
+	response, err := http.ReadResponse(bufio.NewReader(conn), request)
+	if err != nil {
+		panic(err)
+	}
+	dump, err := httputil.DumpResponse(response, true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(dump))
+}
