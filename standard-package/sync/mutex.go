5363dcdaa93c5d5a7184e21d6b6db1f4066079ac
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-08-02 18:20:41 +0900

    Added

---
+++ standard-package/sync/mutex.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"sync"
+)
+
+func main() {
+	var mu sync.Mutex
+	counter := 0
+	for i := 0; i < 1000; i++ {
+		go func() {
+			mu.Lock()
+			defer mu.Unlock()
+			counter++
+			fmt.Print("*")
+		}()
+	}
+
+	time.Sleep(3 * time.Second)
+	fmt.Printf("\n%d\n", counter)
+}
