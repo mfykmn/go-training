bed25a6bdfda592f0d60fe0e49e551419f147550
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-08-08 17:11:29 +0900

    Updated

---
+++ standard-package/testing/unexported-http/export_test.go
@@ -0,0 +1,9 @@
+package unexported // テスト対象と同じパッケージ
+
+func SetBaseURL(s string) (resetFunc func()) {
+	var tmp string
+	tmp, baseURL = baseURL, s
+	return func() {
+		baseURL = tmp
+	}
+}
---
+++ standard-package/testing/unexported-http/mypkg_test.go
@@ -0,0 +1,50 @@
+package unexported_test // テスト対象とは別のパッケージ
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+
+	"github.com/mafuyuk/go-training/standard-package/testing/unexported-http"
+)
+
+func TestGet(t *testing.T) {
+	cases := map[string]struct {
+		n        int
+		hasError bool
+	}{
+		"100": {n: 100},
+		"200": {n: 200},
+	}
+
+	for n, tc := range cases {
+		tc := tc
+		t.Run(n, func(t *testing.T) {
+			var requested bool
+			s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				requested = true
+				if r.FormValue("n") != strconv.Itoa(tc.n) {
+					t.Errorf("param n want %s got %d", r.FormValue("n"), tc.n)
+				}
+				fmt.Fprint(w, `{"value":"hoge"}`)
+			}))
+			defer s.Close()
+			defer unexported.SetBaseURL(s.URL)() // baseURLをモックサーバのものに入れ替え
+
+			cli := unexported.Client{HTTPClient: s.Client()}
+			_, err := cli.Get(tc.n)
+			switch {
+			case err != nil && !tc.hasError:
+				t.Error("unexpected error:", err)
+			case err == nil && tc.hasError:
+				t.Error("expected error has not occurred")
+			}
+
+			if !requested {
+				t.Error("no request")
+			}
+		})
+	}
+}
