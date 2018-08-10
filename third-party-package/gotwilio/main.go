817741614f99faab78cb1785517b373eb5289604
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-08-10 16:39:34 +0900

    Updated

---
+++ third-party-package/gotwilio/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	"github.com/sfreiberg/gotwilio"
+)
+
+// see https://github.com/sfreiberg/gotwilio
+const (
+	accountSid = ""
+	authToken  = ""
+
+	from = "+819000000000" // 受信するtwilio側の電話番号
+	to   = "+819000000000" // 送信する電話番号
+	url  = "https://handler.twilio.com/twiml/dummy" // twiml URL
+)
+func main() {
+	twilio := gotwilio.NewTwilioClient(accountSid, authToken)
+	callbackParams := gotwilio.NewCallbackParameters(url)
+	_, exc, err := twilio.CallWithUrlCallbacks(from, to, callbackParams)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if exc != nil {
+		log.Fatal(exc)
+	}
+}
