d4316fc4b64ea9ec984dfa7b5624dd6da96c5d92
mafuyuk <mafuyuk.m@gmail.com>
2018-08-21 02:17:59 +0900

    Updated

---
+++ third-party-package/zap/cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/mafuyuk/go-training/third-party-package/zap"
+)
+
+//  go run cmd/main.go 1> stdout.log 2> stderr.log
+func main() {
+	zap.New(zap.Debug)
+	zap.Infof("dummy", "標準出力")
+	zap.Errorf("dummy", "標準エラー出力")
+	zap.Fatalf("dummy", "標準エラー出力")
+
+	log.Println("これは標準エラー出力にでる")
+}
--- third-party-package/zap/zap.go
+++ third-party-package/zap/zap.go
@@ -7,6 +7,7 @@ import (
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"os"
 )
 
 type Level int8
@@ -53,34 +54,51 @@ var once sync.Once
 func New(level Level) {
 	once.Do(func() {
 		zapLevel, _ := getZapLevel(level)
-
 		atomicLevel := zap.NewAtomicLevel()
 		atomicLevel.SetLevel(zapLevel)
-		zapConfig := zap.Config{
-			Level:    atomicLevel,
-			Encoding: "json",
-			EncoderConfig: zapcore.EncoderConfig{
-				TimeKey:        "Time",
-				LevelKey:       "Level",
-				NameKey:        "Name",
-				CallerKey:      "Caller",
-				MessageKey:     "Msg",
-				StacktraceKey:  "St",
-				EncodeLevel:    zapcore.CapitalLevelEncoder,
-				EncodeTime:     zapcore.ISO8601TimeEncoder,
-				EncodeDuration: zapcore.StringDurationEncoder,
-				EncodeCaller:   zapcore.ShortCallerEncoder,
-			},
-			OutputPaths:      []string{"stdout"},
-			ErrorOutputPaths: []string{"stderr"},
+
+		encoderConfig := zapcore.EncoderConfig{
+			TimeKey:        "Time",
+			LevelKey:       "Level",
+			NameKey:        "Name",
+			CallerKey:      "Caller",
+			MessageKey:     "Msg",
+			StacktraceKey:  "St",
+			EncodeLevel:    zapcore.CapitalLevelEncoder,
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.StringDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
 		}
 
-		logger, _ := zapConfig.Build()
+		highPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+			return lvl >= zapcore.ErrorLevel
+		})
+		lowPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+			return lvl < zapcore.ErrorLevel
+		})
+
+		consoleDebugging := zapcore.Lock(os.Stdout)
+		consoleErrors := zapcore.Lock(os.Stderr)
+
+		consoleEncoder := zapcore.NewJSONEncoder(encoderConfig)
+
+		core := zapcore.NewTee(
+			zapcore.NewCore(consoleEncoder, consoleErrors, highPriority),
+			zapcore.NewCore(consoleEncoder, consoleDebugging, lowPriority),
+		)
 
 		singletonLogger = &Logger{
-			logger: logger,
+			logger: zap.New(core),
 		}
 	})
+}
+
+func Infof(traceID string, format string, v ...interface{}) {
+	singletonLogger.logger.Info(fmt.Sprintf(format, v...), zap.String("trace_id", traceID))
+}
+
+func Errorf(traceID string, format string, v ...interface{}) {
+	singletonLogger.logger.Error(fmt.Sprintf(format, v...), zap.String("trace_id", traceID))
 }
 
 func Fatalf(traceID string, format string, v ...interface{}) {
