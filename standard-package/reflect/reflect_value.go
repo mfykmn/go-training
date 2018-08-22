fc0c35c06a5643c50709670c8de124b96fe548ff
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-08-22 16:38:58 +0900

    Updated

--- standard-package/reflect/reflect_value.go
+++ standard-package/reflect/reflect_value.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	rv1 := reflect.ValueOf(1)
-	rv2 := reflect.ValueOf("Hello World")
-	rv3 := reflect.ValueOf([]byte{0xa,0xd})
-	rv4 := reflect.ValueOf(make(chan struct{}))
+	//rv2 := reflect.ValueOf("Hello World")
+	//rv3 := reflect.ValueOf([]byte{0xa,0xd})
+	//rv4 := reflect.ValueOf(make(chan struct{}))
 
 	fmt.Println(rv1.Int())
 	//fmt.Println(rv2.Int()) #panic
