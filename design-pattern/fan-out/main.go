090e062f6b1570b981a0e9a95efe83f2b8d3889e
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-08-22 20:22:03 +0900

    Updated

--- design-pattern/fan-out/main.go
+++ design-pattern/fan-out/main.go
@@ -1,14 +1,41 @@
-package fan_out
+package main
+
+import (
+	"reflect"
+	"fmt"
+)
+
+// lean web db press 105
 
 func main() {
+	src := make(chan interface{})
+	dst1 := make(chan interface{})
+	dst2 := make(chan interface{})
+
+	var channels = []chan interface{}{dst1, dst2}
+
+	go fanout(src, channels)
+
+	src <- "hello"
+
+	fmt.Printf("dst1: %s\n", <-dst1)
+	fmt.Printf("dst2: %s\n", <-dst2)
+}
+
+func fanout(src <-chan interface{}, dstChs []chan interface{}) {
 	for {
 		data := <- src
-		select {
-		case <- ctx.Done():
-		  return ctx.Err()
-		case dst1<-data:
-		case dst2<-data:
-		case dst3<-data:
+		cases := make([]reflect.SelectCase, len(dstChs))
+		for i, ch := range dstChs {
+			cases[i] = reflect.SelectCase{
+				Chan: reflect.ValueOf(ch),
+				Dir: reflect.SelectSend,
+				Send: reflect.ValueOf(data),
+			}
+		}
+		for len(cases) > 0 {
+			chosen, _, _ := reflect.Select(cases)
+			cases = append(cases[:chosen], cases[chosen+1:]...)
 		}
 	}
 }
