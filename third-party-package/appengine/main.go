e4a215c9fd553890d30c241220b085417d17d8fe
kamono-mafuyu <kamono-mafuyu@dmm.com>
2019-05-27 19:25:40 +0900

    Refactor

--- third-party-package/appengine/main.go
+++ third-party-package/appengine/main.go
@@ -6,57 +6,35 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/go-chi/chi"
 )
 
+var port = os.Getenv("PORT")
+
 func main() {
+	// Init
 	db, err := newDB()
 	if err != nil {
 		panic(err)
 	}
-
 	ctx := context.Background()
   scheduler, err := newScheduler(ctx)
 	if err != nil {
 		panic(err)
 	}
 
+  // Routing
 	r := chi.NewRouter()
 	r.Get("/", helloHandler)
-	r.Get("/db", func(w http.ResponseWriter, r *http.Request) {
-		res, err := db.Show()
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Failed show db: %v", err), 500)
-			return
-		}
-		w.Write(res)
-	})
+	r.Get("/db", dbHandlerFunc(db))
+	r.Get("/db/schedule", scheduleHandlerFunc(ctx, scheduler))
 
-	r.Get("/db/schedule", func(w http.ResponseWriter, r *http.Request) {
-		jst, err := time.LoadLocation("Asia/Tokyo")
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Failed get location: %v", err), 500)
-			return
-		}
-		if err := scheduler.Reserve(ctx, time.Now().In(jst).Add(time.Duration(3) * time.Minute)); err != nil {
-			http.Error(w, fmt.Sprintf("Failed create schdule job: %v", err), 500)
-			return
-		}
-		fmt.Fprintln(w, "Scheduled")
-	})
-
-	port := os.Getenv("PORT")
+	// Serve
 	if port == "" {
 		port = "8080"
 		log.Printf("Defaulting to port %s", port)
 	}
-
 	log.Printf("Listening on port %s", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), r))
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello")
 }
