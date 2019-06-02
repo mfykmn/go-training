86ebdbbbc3343e3fd21169c05493fe5b489e6d0c
mafuyuk <mafuyuk.m@gmail.com>
2019-06-02 17:49:14 +0900

    Fixed

--- third-party-package/gorm/main.go
+++ third-party-package/gorm/main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"time"
 )
 
 func gormConnect() *gorm.DB {
-	DBMS     := "mysql"
-	USER     := "root"
-	PASS     := "root"
+	DBMS := "mysql"
+	USER := "root"
+	PASS := "root"
 	PROTOCOL := "tcp(0.0.0.0:3306)"
-	DBNAME   := "test"
+	DBNAME := "test"
 
-	CONNECT := USER+":"+PASS+"@"+PROTOCOL+"/"+DBNAME
-	db,err := gorm.Open(DBMS, CONNECT)
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
+	db, err := gorm.Open(DBMS, CONNECT)
 
 	if err != nil {
 		panic(err)
@@ -22,22 +24,69 @@ func gormConnect() *gorm.DB {
 }
 
 type event struct {
-	Id          int    `json:id sql:AUTO_INCREMENT`
-	User_Id     int    `json:user_id`
-	Summary     string `json:summary`
-	Dtstart     string `json:dtstart`
-	Dtend       string `json:dtend`
-	Description string `json:description`
-	Year        int    `json:year`
-	Month       int    `json:month`
-	Day         int    `json:day`
+	Id        int    `json:id sql:AUTO_INCREMENT`
+	Name      string `json:name`
+	DeletedAt *time.Time
 }
 
-
 // see http://doc.gorm.io/crud.html
-func main(){
+func main() {
 	db := gormConnect()
+	db.AutoMigrate(&event{})
+	err := CreateEvents(db)
+	if err != nil {
+		fmt.Printf("%#v", err)
+	}
 
-	db.CreateTable(&event{})
-	db.Begin()
+	if err := DeleteEvent(db); err != nil {
+		fmt.Printf("%#v", err)
+	}
+}
+
+func CreateEvents(db *gorm.DB) error {
+	tx := db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	if err := tx.Error; err != nil {
+		return err
+	}
+
+	if err := tx.Create(&event{Name: "A"}).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Create(&event{Name: "B"}).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return tx.Commit().Error
+}
+
+func DeleteEvent(db *gorm.DB) error {
+	tx := db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	if err := tx.Error; err != nil {
+		return err
+	}
+
+	where := event{
+		Id: 12,
+	}
+	if err := tx.Where(where).Delete(&event{}).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return tx.Commit().Error
 }
