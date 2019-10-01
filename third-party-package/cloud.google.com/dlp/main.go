ff40bf49d1d771dfc3aa8f8e839d5663bd56e4cd
kamono-mafuyu <kamono-mafuyu@dmm.com>
2019-10-01 13:54:10 +0900

    update

--- third-party-package/cloud.google.com/dlp/main.go
+++ third-party-package/cloud.google.com/dlp/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 
 	dlp "cloud.google.com/go/dlp/apiv2"
 	dlppb "google.golang.org/genproto/googleapis/privacy/dlp/v2"
@@ -30,6 +31,8 @@ TO:hoge@hoge.com
 `
 
 	inspectString(projectID, client, infoTypes, message)
+	fmt.Println("-----------------------------")
+	masking(projectID, client, infoTypes, message)
 }
 
 func inspectString(projectID string, client *dlp.Client, infoTypes []string, input string) {
@@ -79,4 +82,51 @@ func inspectString(projectID string, client *dlp.Client, infoTypes []string, inp
 	for _, result := range resp.GetResult().GetFindings() { // 検出されたものはFinding
 		fmt.Println(resultFormatter(input, result))
 	}
+}
+
+func masking(projectID string, client *dlp.Client, infoTypes []string, input string) {
+	// 検出機密種別をdlppb.InfoTypeに変換
+	var i []*dlppb.InfoType
+	for _, it := range infoTypes {
+		i = append(i, &dlppb.InfoType{Name: it})
+	}
+
+	// リクエストのデータを構築
+	req := &dlppb.InspectContentRequest{
+		Parent: "projects/" + projectID, // 必ずprojects/を前につける
+		InspectConfig: &dlppb.InspectConfig{ // 検出条件など
+			InfoTypes: i,
+		},
+		Item: &dlppb.ContentItem{ // 検出対象
+			DataItem: &dlppb.ContentItem_Value{
+				Value: input,
+			},
+		},
+	}
+
+	resp, err := client.InspectContent(context.Background(), req) // 検出を実行
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(input)
+	fmt.Println(mask(input, resp.GetResult().GetFindings(), 0))
+}
+
+func mask(input string, results []*dlppb.Finding, index int) (maskedStr string) {
+	if len(results) <= index {
+		return input
+	}
+
+	target := getMaskTarget(input, results[index])
+	maskedStr = strings.Replace(input, target, strings.Repeat("*", len(target)), -1)
+
+	index++
+	return mask(maskedStr, results, index)
+}
+
+func getMaskTarget(input string, result *dlppb.Finding) string {
+	start := result.GetLocation().GetCodepointRange().GetStart()
+	end := result.GetLocation().GetCodepointRange().GetEnd()
+	return string([]rune(input)[start:end])
 }
