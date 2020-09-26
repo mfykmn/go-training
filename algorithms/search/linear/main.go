7677ca92e15a9b0e1219781613ba16c3c80934dd
mfykmn <mfykmn@gmail.com>
2020-09-26 23:24:12 +0900

    Updated

--- algorithms/search/linear/main.go
+++ algorithms/search/linear/main.go
@@ -12,16 +12,16 @@ func main() {
 
 	fmt.Println("----------------exists-----------------")
 	key := 5
-	fmt.Printf("val:%s, address:%s\n", a[*linearSearch(&a, key)], &a[*linearSearch(&a, key)])
+	fmt.Printf("val:%s, address:%s\n", a[*linearSearch(a, key)], &a[*linearSearch(a, key)])
 
 	fmt.Println("----------------nil-----------------")
 	key = 4
-	fmt.Printf("%v\n", linearSearch(&a, key))
+	fmt.Printf("%v\n", linearSearch(a, key))
 }
 
 // linearSearchはランダムな整数列に対する探索
-func linearSearch(a *[]int, key int) *int {
-	for i, v := range *a {
+func linearSearch(a []int, key int) *int {
+	for i, v := range a {
 		if v == key {
 			return &i
 		}
