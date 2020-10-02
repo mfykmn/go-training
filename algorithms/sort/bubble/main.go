799638e9678295a4fe928df13e3e580be05be78b
mfykmn <mfykmn@gmail.com>
2020-10-02 11:16:02 +0900

    Updated

--- algorithms/sort/bubble/main.go
+++ algorithms/sort/bubble/main.go
@@ -1,3 +1,38 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	// ランダムな整数列
+	a := []int{7, 9, 12, 4, 5}
+
+	fmt.Println("----------------before-----------------")
+	for i, v := range a {
+		fmt.Printf("val:%s, address:%s\n", v, &a[i])
+	}
+
+	bubbleSort(a)
+
+	fmt.Println("----------------after-----------------")
+	for i, v := range a {
+		fmt.Printf("val:%s, address:%s\n", v, &a[i])
+	}
+}
+
+func bubbleSort(a []int) {
+	n := len(a)
+	for i := 0; i <= n-2; i++ {
+		for j := n - 1; j >= i+1; j-- {
+			if a[j-1] > a[j] {
+				swap(&a[j-1], &a[j])
+			}
+		}
+	}
+}
+
+func swap(a, b *int) {
+	var t int
+	t = *a
+	*a = *b
+	*b = t
+}
